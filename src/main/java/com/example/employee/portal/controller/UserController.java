5bdca791bc975ce62b05691f650c0192079eaca4
md.hafizurrahmanarfin <hafizur.arfin@margin.edge.com>
2024-04-27 23:07:04 +0600

    thymeleaf

---
+++ src/main/java/com/example/employee/portal/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.employee.portal.controller;
+
+import com.example.employee.portal.domain.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String user(@ModelAttribute UserDto user, Model model) {
+        model.addAttribute("message", "User registered successfully!");
+        model.addAttribute("user", new UserDto());
+        System.out.println(user.getName());
+        return "register";
+    }
+}
---
+++ src/main/java/com/example/employee/portal/domain/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.employee.portal.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String name;
+
+}
